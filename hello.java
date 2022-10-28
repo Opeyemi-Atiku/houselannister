261c243aaac7ae040a8cc3844fd6de99ab8bc5a7
Opeyemi-Atiku <31370816+Opeyemi-Atiku@users.noreply.github.com>
2022-10-28 14:26:12 +0100

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,9 @@
+/*
+* The HelloWorld Java program
+* prints "Hello World!" on the computer screen
+*/
+class hello {
+  public static void main(String[] args) {
+   System.out.println("Hello, World!");
+  }
+}
