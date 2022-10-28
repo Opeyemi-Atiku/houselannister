f7240a4938672a7bf6f4de4545b6e3b2a4c74125
Opeyemi-Atiku <31370816+Opeyemi-Atiku@users.noreply.github.com>
2022-10-28 14:39:23 +0100

    Delete hello.java

--- hello.java
+++
@@ -1,9 +0,0 @@
-/*
-* The HelloWorld Java program
-* prints "Hello World!" on the computer screen
-*/
-class hello {
-  public static void main(String[] args) {
-   System.out.println("Hello, World!");
-  }
-}
